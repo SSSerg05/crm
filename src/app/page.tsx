d5e9f912635e08df82262267f65abfd51ea392a3
SSSerg05 <ssserg05@gmail.com>
2026-05-29 17:30:40 +0300

    add tailwind

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main>
-        <h1>Home Page</h1>
+        <h1 className="text-xl">Home Page</h1>
         <ActiveLabel>Active Label</ActiveLabel>
         <NotActiveLabel>Not Active Label</NotActiveLabel>  
       </main>
