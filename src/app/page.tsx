416e692993aecafc14f1035427378079bda5a0a6
SSSerg05 <ssserg05@gmail.com>
2026-05-29 15:54:26 +0300

    add module active+not-active label

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,13 @@
+import ActiveLabel from "@/app/components/active-label";
+import NotActiveLabel from "@/app/components/not-active-label";
+
 export default function Home() {
   return (
     <div className="flex flex-col flex-1 items-center justify-center bg-zinc-50 font-sans dark:bg-black">
       <main>
         <h1>Home Page</h1>
+        <ActiveLabel>Active Label</ActiveLabel>
+        <NotActiveLabel>Not Active Label</NotActiveLabel>  
       </main>
     </div>
   );
